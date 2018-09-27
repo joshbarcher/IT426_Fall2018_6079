40b2ea670600d2c1deadf2bd68629d111b0c91ff
Josh Archer <jarcher@greenriver.edu>
2018-09-27 12:16:43 -0700

    Added a "Hello World" example of Java FX. Also added an example of switching scenes.

---
+++ Java FX Intro/src/intro/HelloWorld.java
@@ -0,0 +1,52 @@
+package intro;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+
+/**
+ * This is our first Java FX app.
+ *
+ * @author Josh Archer
+ * @version 1.0
+ */
+public class HelloWorld extends Application
+{
+    public static final int WIN_SIZE = 300;
+    public static final int DEF_FONT_SIZE = 30;
+
+    @Override
+    public void start(Stage stage)
+    {
+        //perform a few setup tasks
+        stage.setTitle("Welcome to Java FX");
+        stage.setScene(buildScene());
+        stage.show();
+    }
+
+    /**
+     * Assembles our UI.
+     * @return a new scene object
+     */
+    public Scene buildScene() //assemble our UI
+    {
+        //create a panel and an element to place inside of it
+        VBox panel = new VBox();
+        Text info = new Text("Hello world!");
+
+        //add a few styles
+        panel.setAlignment(Pos.CENTER);
+        info.setFont(Font.font("Verdana", FontWeight.BOLD, DEF_FONT_SIZE));
+
+        panel.getChildren().add(info);
+
+        return new Scene(panel, WIN_SIZE, WIN_SIZE);
+    }
+}
---
+++ Java FX Intro/src/switchingscenes/StoryBook.java
@@ -0,0 +1,77 @@
+package switchingscenes;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class StoryBook extends Application
+{
+    public static final int WIN_SIZE = 300;
+
+    private Stage stage;
+    private String[] headers = {"Part #1", "Part #2", "Part #3"};
+    private String[] bodies = {
+            "Today, in the news, unicorns invade Seattle...",
+            "The killer clowns were there to stop them...",
+            "The clowns prevailed... The end"
+    };
+
+    @Override
+    public void start(Stage stage)
+    {
+        //record the stage for use later
+        this.stage = stage;
+
+        stage.setTitle("Welcome to Java FX");
+        stage.setScene(getWaitScreen());
+        stage.show();
+
+        //wait a few seconds, to simulate load times, then display our story
+        KeyFrame frame = new KeyFrame(Duration.millis(3000),
+                event -> { stage.setScene(getStoryScreen(0)); });
+        Timeline animation = new Timeline(frame);
+        animation.play();
+    }
+
+    public Scene getWaitScreen()
+    {
+        //show a progress indicator and a message
+        ProgressIndicator progress = new ProgressIndicator();
+        Text message = new Text("Please wait...");
+
+        return getSceneWithVBox(progress, message);
+    }
+
+    public Scene getSceneWithVBox(Node... elements)
+    {
+        VBox panel = new VBox();
+        panel.getChildren().addAll(elements);
+
+        return new Scene(panel, WIN_SIZE, WIN_SIZE);
+    }
+
+    public Scene getStoryScreen(int index)
+    {
+        Text header = new Text(headers[index]);
+        Text body = new Text(bodies[index]);
+
+        if (index != headers.length - 1)
+        {
+            //send the user to the next screen if they click the button
+            Button button = new Button("Next");
+            button.setOnAction(event -> { stage.setScene(getStoryScreen(index + 1)); });
+
+            return getSceneWithVBox(header, body, button);
+        }
+
+        return getSceneWithVBox(header, body);
+    }
+}
